7c8f11805cd9b1a3b10096a2959a7a091edbc8cc
Suman Kumar Ghorai <sumanghorai000000000@gmail.com>
2024-10-05 22:06:22 +0530

    Footer start added build

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import React from 'react';
+
+
+import usa from '../../assets/image/usa.svg';
+import gpay from '../../assets/image/gpay.svg';
+import opay from '../../assets/image/opay.svg';
+import paypal from '../../assets/image/paypal.svg';
+import apple from '../../assets/image/applepay.svg';
+import amex from '../../assets/image/amex.svg';
+import insta from '../../assets/image/insta.svg';
+import linkden from '../../assets/image/linkden.svg';
+import voda from '../../assets/image/voda.svg';
 
 function Footer() {
   return (
-    <div>
-      <h1>Footer</h1>
-    </div>
-  )
+    <>
+      <div className='footer'>
+        <div className='footer-upper'>
+          <div className='upper-left'>
+            <h4>BE THE FIRST TO KNOW</h4>
+            <h2>Sign up for updates from metta muse.</h2>
+          </div>
+        </div>
+        <div className='footer-lower'>
+          {/* Add footer content here */}
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default Footer
+export default Footer;
